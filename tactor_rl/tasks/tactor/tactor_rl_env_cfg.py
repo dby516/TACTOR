fa2e946ab230c0039e990392be3a1e2af2dfb43c
unknown <bd2747@columbia.edu>
2025-04-28 16:27:00 -0400

    Updated tactor_rl

--- tactor_rl/tasks/tactor/tactor_rl_env_cfg.py
+++ tactor_rl/tasks/tactor/tactor_rl_env_cfg.py
@@ -2,14 +2,32 @@ from isaaclab_assets.robots.ur10e_tactip import UR10e_TACTIP_CFG, TACTIP_CFG
 from isaaclab_assets.robots.allegro import ALLEGRO_HAND_CFG
 
 import isaaclab.sim as sim_utils
-from isaaclab.assets import ArticulationCfg, RigidObjectCfg
+from isaaclab.assets import ArticulationCfg, RigidObjectCfg, AssetBaseCfg
 from isaaclab.sensors import ContactSensorCfg
 from isaaclab.envs import DirectRLEnvCfg
 from isaaclab.scene import InteractiveSceneCfg
-from isaaclab.sim import PhysxCfg, SimulationCfg, UsdFileCfg
+from isaaclab.sim import PhysxCfg, SimulationCfg, UsdFileCfg, CuboidCfg
 from isaaclab.sim.spawners.materials.physics_materials_cfg import RigidBodyMaterialCfg
 from isaaclab.utils import configclass
 from isaaclab.utils.assets import ISAAC_NUCLEUS_DIR
+
+@configclass
+class TactorEnvSceneCfg(InteractiveSceneCfg):
+    # Robot
+    robot = TACTIP_CFG.replace(prim_path="/World/envs/env_.*/Robot")
+
+    # Object: replace it with ShapeNet object
+    object = AssetBaseCfg(
+        prim_path = "/World/envs/env_.*/Object",
+        spawn = sim_utils.CuboidCfg(
+            size=(0.5, 0.5, 0.5),
+            collision_props=sim_utils.CollisionPropertiesCfg(),
+            visual_material=sim_utils.PreviewSurfaceCfg(diffuse_color=(0.5, 1.0, 0.0)),
+        ),
+        init_state = AssetBaseCfg.InitialStateCfg(pos=(0, 0, 0)),
+    )
+    
+    # Sensors
 
 @configclass
 class TactorEnvCfg(DirectRLEnvCfg):
@@ -24,58 +42,31 @@ class TactorEnvCfg(DirectRLEnvCfg):
 
     # simulation
     sim: SimulationCfg = SimulationCfg(
-        dt=1 / 120,
+        dt=1/1200,
         render_interval=decimation,
-        physics_material=RigidBodyMaterialCfg(
-            static_friction=1.0,
-            dynamic_friction=1.0,
-        ),
         physx=PhysxCfg(
-            bounce_threshold_velocity=0.2,
-        ),
+            # min_position_iteration_count=2,
+            # max_position_iteration_count=2,
+            # min_velocity_iteration_count=0,
+            # max_velocity_iteration_count=0,
+            enable_ccd=False,
+            enable_stabilization=True,
+            enable_enhanced_determinism=False,
+        )
     )
 
-    # robot
-    robot_cfg: ArticulationCfg = TACTIP_CFG.replace(prim_path="/World/envs/env_.*/Robot")
-    
-    # contact sensors
-    contact_sensors: list[ContactSensorCfg] = [
-        ContactSensorCfg(
+    # scene
+    for i in range(64): # Add contact sensors
+        setattr(TactorEnvSceneCfg, f"contact_sensor_{i}", ContactSensorCfg(
             prim_path=f"/World/envs/env_.*/Robot/sensor_{i}",
             update_period=0.0,
             history_length=6,
             debug_vis=True,
-            track_pose=True
-        )
-        for i in range(64)
-    ]
+            track_pose=True,
+        ))
+    scene: TactorEnvSceneCfg = TactorEnvSceneCfg(num_envs=6, env_spacing=2.5, replicate_physics=True)
 
-    # Object: replace it with ShapeNet object
-    object_cfg: RigidObjectCfg = RigidObjectCfg(
-        prim_path="/World/envs/env_.*/Object",
-        spawn=sim_utils.CuboidCfg(
-            size=(0.5, 0.5, 0.5),
-            rigid_props=sim_utils.RigidBodyPropertiesCfg(disable_gravity=True, max_linear_velocity=0.0, max_angular_velocity=0.0), 
-            mass_props=sim_utils.MassPropertiesCfg(mass=100.0),
-            collision_props=sim_utils.CollisionPropertiesCfg(),
-            physics_material=sim_utils.RigidBodyMaterialCfg(static_friction=1.0),
-            visual_material=sim_utils.PreviewSurfaceCfg(diffuse_color=(0.5, 1.0, 0.0)),
-        ),
-        # spawn=sim_utils.UsdFileCfg(usd_path="../../../../assets/objects/lamp.usd")
-        init_state=RigidObjectCfg.InitialStateCfg(pos=(0.0, 0.0, 0.0))
-    )
-
-    # table
-    table_cfg: RigidObjectCfg = RigidObjectCfg(
-        prim_path="/World/envs/env_.*/Table",
-        spawn=sim_utils.UsdFileCfg(
-            usd_path=f"{ISAAC_NUCLEUS_DIR}/Props/Mounts/ThorlabsTable/table_instanceable.usd",
-        ),
-    )
-
-    # scene
-    scene: InteractiveSceneCfg = InteractiveSceneCfg(num_envs=6, env_spacing=2.5, replicate_physics=True)
-
+    
     # reset (no joint reset needed for TacTip)
     reset_position_noise = 0.01
     reset_dof_pos_noise = 0.0
